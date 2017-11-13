eb46b1824cab09e5bb4d70cb1368b69952ced882
Unknown <richardpanda1993@gmail.com>
2017-11-13 15:45:57 -0800

    Add User-Agent header

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"strings"
 
 	"golang.org/x/net/html"
@@ -26,21 +27,30 @@ func extractTitle(n *html.Node) string {
 }
 
 func main() {
-	// resp, err := http.Get("http://www.imdb.com/title/tt4574334/?ref_=nv_sr_1")
-
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
-
-	// defer resp.Body.Close()
-
-	// bodyBytes, err := ioutil.ReadAll(resp.Body)
-
-	bodyBytes, err := ioutil.ReadFile("stranger-things.html")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", "http://www.imdb.com/title/tt4574334/?ref_=nv_sr_1", nil)
 
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	req.Header.Add("User-Agent", "Scrapher, a friendly web scraper. Code can be found at https://github.com/richardpanda/scrapher.")
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	// bodyBytes, err := ioutil.ReadFile("index.html")
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
 	bodyString := string(bodyBytes)
 	doc, err := html.Parse(strings.NewReader(bodyString))
