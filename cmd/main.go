cdd108dd8ff36d503e48180d57ee5c1502560a87
Unknown <richardpanda1993@gmail.com>
2017-11-13 18:27:49 -0800

    Extract title and year

--- cmd/main.go
+++ cmd/main.go
@@ -2,62 +2,51 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
+	"os"
+	"regexp"
 	"strings"
 
-	"golang.org/x/net/html"
+	"github.com/PuerkitoBio/goquery"
 )
 
-func extractTitle(n *html.Node) string {
-	for _, attr := range n.Attr {
-		if attr.Key == "class" && attr.Val == "title_wrapper" {
-			return strings.TrimSpace(n.FirstChild.NextSibling.FirstChild.Data)
-		}
-	}
-
-	for c := n.FirstChild; c != nil; c = c.NextSibling {
-		if title := extractTitle(c); title != "" {
-			return title
-		}
-	}
-
-	return ""
-}
-
 func main() {
-	client := &http.Client{}
-	req, err := http.NewRequest("GET", "http://www.imdb.com/title/tt4574334/?ref_=nv_sr_1", nil)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	req.Header.Add("User-Agent", "Scrapher, a friendly web scraper. Code can be found at https://github.com/richardpanda/scrapher.")
-
-	resp, err := client.Do(req)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer resp.Body.Close()
-
-	bodyBytes, err := ioutil.ReadAll(resp.Body)
-
-	// bodyBytes, err := ioutil.ReadFile("index.html")
+	// client := &http.Client{}
+	// req, err := http.NewRequest("GET", "http://www.imdb.com/title/tt4574334/?ref_=nv_sr_1", nil)
 
 	// if err != nil {
 	// 	log.Fatal(err)
 	// }
 
-	bodyString := string(bodyBytes)
-	doc, err := html.Parse(strings.NewReader(bodyString))
+	// req.Header.Add("User-Agent", "Scrapher, a friendly web scraper. Code can be found at https://github.com/richardpanda/scrapher.")
+
+	// resp, err := client.Do(req)
+
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	// defer resp.Body.Close()
+
+	// bodyBytes, err := ioutil.ReadAll(resp.Body)
+
+	re := regexp.MustCompile(`(.+) \((\d{4})\)`)
+	f, err := os.Open("index.html")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Print(extractTitle(doc))
+	doc, err := goquery.NewDocumentFromReader(f)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	matches := re.FindStringSubmatch(strings.TrimSpace(doc.Find(".title_wrapper > h1").First().Text()))
+	title := matches[1]
+	year := matches[2]
+
+	fmt.Println(title)
+	fmt.Println(year)
 }
