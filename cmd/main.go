5f78e598a298400fe6a10488283132744a041d66
Unknown <richardpanda1993@gmail.com>
2017-11-20 17:36:44 -0800

    Scrape IMDB and Rotten Tomatoes concurrently

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
+
+	"github.com/richardpanda/scrapher/src/scraper"
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/postgres"
 	"github.com/richardpanda/scrapher/src/models"
-	"github.com/richardpanda/scrapher/src/scraper"
+	"github.com/richardpanda/scrapher/src/scraper/imdb"
 	"github.com/richardpanda/scrapher/src/scraper/rottentomatoes"
 )
 
 func main() {
+	var wg sync.WaitGroup
 	dbUser := os.Getenv("DB_USER")
 	dbName := os.Getenv("DB_NAME")
 	connectionString := fmt.Sprintf("user=%s dbname=%s sslmode=disable", dbUser, dbName)
@@ -26,8 +30,17 @@ func main() {
 
 	db.AutoMigrate(&models.Movie{})
 
-	// i := imdb.New("http://www.imdb.com/title/tt0468569")
-	// scraper.Start(db, i)
+	i := imdb.New("http://www.imdb.com/title/tt0468569")
 	rt := rottentomatoes.New("https://www.rottentomatoes.com/m/the_dark_knight")
-	scraper.Start(db, rt)
+	scrapers := []scraper.Scraper{i, rt}
+
+	for _, s := range scrapers {
+		wg.Add(1)
+		go func(s scraper.Scraper) {
+			defer wg.Done()
+			scraper.Start(db, s)
+		}(s)
+	}
+
+	wg.Wait()
 }
