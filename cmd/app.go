1553ac31fcdd2a21496a4ca9816a048ef405fccc
Unknown <richardpanda1993@gmail.com>
2017-11-30 15:20:09 -0800

    Remove unused files

--- cmd/app.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "github.com/richardpanda/scrapher/queue"
-
-type App struct {
-	imdb *queue.Queue
-	rt   *queue.Queue
-}
--- cmd/error.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func printError(e <-chan error) {
-	for err := range e {
-		fmt.Println(err)
-	}
-}
